ee98c7f86d41bf639327f051a87337dfba264525
CarlRJBarton <carl.barton.artwork@outlook.com>
2025-06-17 18:33:14 +0100

    Upload Day One

---
+++ 1_Day_One_Activities_Completed/DayOne_Activity_2.py
@@ -0,0 +1,7 @@
+print("dogs".upper()) # Capitalises everything within the string
+print("DOGS".lower()) # Makes everything within the string lowercase
+print("dogs and cats".capitalize()) # Just capitalises the first letter of the string
+print("sharks".count("s")) # Provides the total amount of that character within the string
+print("sharks".find("s")) # The .find() method only returns the index of the first occurrence
+print("cats".replace("c", "b")) # Replaces any letter 'c' with 'b'
+print("cats".strip("s")) # In this case removes any 's' from the string
